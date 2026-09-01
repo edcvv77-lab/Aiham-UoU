29082159d3fa2c61cc7489cf144ee2cc0f90edb3
edcvv77-lab <edcvv77@gmail.com>
2026-09-01 10:08:52 +0300

    refactor: improve theme architecture

--- mobile/lib/core/theme/app_theme.dart
+++ mobile/lib/core/theme/app_theme.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData dark() {
+  static ThemeData get dark {
     return ThemeData(
       brightness: Brightness.dark,
       useMaterial3: true,
-      scaffoldBackgroundColor: const Color(0xFF090909),
+      scaffoldBackgroundColor: const Color(0xFF080808),
+      colorScheme: const ColorScheme.dark(),
     );
   }
 }
