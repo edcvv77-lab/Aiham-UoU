db2182d110207ff7d7b3f2b8cfff562940457ec9
edcvv77-lab <edcvv77@gmail.com>
2026-09-01 10:22:43 +0300

    Add Firebase service foundation

---
+++ mobile/lib/core/firebase/firebase_service.dart
@@ -0,0 +1,6 @@
+class FirebaseService {
+  static Future<void> initialize() async {
+    // Firebase initialization will be connected here.
+    // Kept isolated to allow future configuration without changing app layers.
+  }
+}
